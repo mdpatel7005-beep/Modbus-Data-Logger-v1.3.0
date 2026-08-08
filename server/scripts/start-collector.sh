82e318c1a73cd14bb0dffdd7a21133349e658835
mdpatel7005-beep <mdpatel7005@gmail.com>
2026-08-08 12:46:28 +0530

    Fix startup script: run dev:collector from project root

--- server/scripts/start-collector.sh
+++ server/scripts/start-collector.sh
@@ -4,4 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 cd /root/Modbus-Data-Logger-v1.3.0
-npm --prefix server run dev:collector
+
+# Run dev:collector directly from project root (uses --prefix server)
+npm run dev:collector
